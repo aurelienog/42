1b963c6adf7568cf45930a972ee4753113adab26
Aurelie Nogueira <aurelienog@gmail.com>
2025-12-22 14:03:18 +0100

    added ft_strlen.c

---
+++ piscine/C01/ex06/ft_strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	printf("%d", ft_strlen("hello world"));
+	return (0);
+}
