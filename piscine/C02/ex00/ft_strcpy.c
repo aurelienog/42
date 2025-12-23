0d63f3b96b91ed3c915b10514bddc206f57a4d7a
Aurelie Nogueira <aurelienog@gmail.com>
2025-12-23 18:18:47 +0100

    added ft_strcpy.c

---
+++ piscine/C02/ex00/ft_strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while(src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char	ptr[6];
+	printf("%s", ft_strcpy(ptr, "hello"));
+	return (0);
+}
+
