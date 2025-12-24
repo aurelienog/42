fd969454ab6ef438ede96ceae375a18309e00695
Aurelie Nogueira <aurelienog@gmail.com>
2025-12-24 16:11:29 +0100

    added ft_str_is_lowercase.c

---
+++ piscine/C02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*ptr;
+
+	if (argc > 1)
+	{
+		ptr = argv[1];
+		printf("%d", ft_str_is_lowercase(ptr));
+	}
+	return (0);
+}
